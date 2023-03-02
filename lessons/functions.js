7c0d298ddb2768810d3549e46a99c07af8901314
hardos <118268934+hard0s@users.noreply.github.com>
2023-03-02 08:35:59 +0300

    лекция про функции и лекция дани про функции

---
+++ lessons/functions.js
@@ -0,0 +1,45 @@
+"use strict";
+/*const deckArr = ["король ","5","7","6","10","дама","валет","туз","2","3","4","5","10","валет","король"]
+let check = false
+
+
+function deck(deckArr) {
+   for (let i = 0; i < deckArr.length; i++) {
+    if (deckArr[i] == "туз"){
+        console.log(`туз был ${i + 1} в колоде`)
+        check = true
+        break; 
+    }
+   }
+    if (check === false){
+        console.log("туза нема")
+    }
+}
+deck(deckArr)*/
+/*let x = false
+let arr = [-1,2, -36, 5, 40, 12, 1]
+let positive = []
+function isPositive(item){
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] > 0){
+            let x = true
+        }
+        else{
+            let x = false
+        }
+    }
+    
+}
+function arrayWrite(){
+    for(let i = 0; i < arr.length; i++){
+    if (isPositive(arr[i]) == true){
+        positive.push(arr[i])
+    }
+}
+}
+console.log(isPositive(arr))
+console.log(arrayWrite())*/
+
+//лекция Дани про функции functional decloration
+
+
