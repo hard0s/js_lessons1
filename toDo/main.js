8059715647b701de01b5cfd781c6d23f6402b5ca
hard0s <118268934+hard0s@users.noreply.github.com>
2023-04-03 14:02:38 +0300

    переименовал и сменил папку

--- toDo/main.js
+++ toDo/main.js
@@ -8,26 +8,27 @@ let taskPlace = document.querySelector('.tasks')
 function btnClick() {
     if (placeholder.value.length == 0) {
         alert("Пожалуйста напишите свое дело")
-    }
-    else {
-        
+    } else {
+
         taskPlace.innerHTML += ` 
             <div class="task">
                 <span id="taskname">
                     ${placeholder.value}
                 </span>
                 <button type="button" id="btnDel" class="btn btn-del">Удалить</button>
+                <button type="button" id="btnDon" class="btn btn-done" >Сделанно</button>
                 
             </div>
         `;
+        let taskDone = addEventListener("click",)
+        let taskDel = document.querySelectorAll('#btnDel');
 
-        let taskDel = document.querySelectorAll('#btnDel'); 
         for (let i = 0; i < taskDel.length; i++) {
             taskDel[i].onclick = function() {
                 this.parentNode.remove();
             }
         }
-       
+
     }
 }
 //не знаю как сделать кнопку которая будет менять цвет
